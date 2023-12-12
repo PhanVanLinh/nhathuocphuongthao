e6e1c3142808c919783f3bbf59caf39b185d2ed3
vanlinh.phan <vanlinh.phan@okg.com>
2023-12-12 12:55:37 +0800

    first commit for web

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+  res.render('pages/home', {title: "Quầy thuốc Phương Thảo", zalo: "0036123456"});
+});
+
+// about page
+app.get('/about', function(req, res) {
+  res.render('pages/about');
+});
+
+app.listen(4000);
+console.log('Server is listening on port 4000');
