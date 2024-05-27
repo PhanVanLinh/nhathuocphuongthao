0c3014d64966aee62e20049dcd27172f24b0d027
PhanVanLinh <phanvanlinh.94vn@gmail.com>
2024-05-27 08:29:06 +0800

    Add action icon

---
+++ views/pages/home/backup.js
@@ -0,0 +1,18 @@
+<div class="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
+    <div
+        class="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
+        <a href="#">Thuốc <span aria-hidden="true">&rarr;</span></a>
+        <a href="#">Thực phẩm chức năng <span aria-hidden="true">&rarr;</span></a>
+        <a href="#">Thiết bị, dụng cụ y tế <span aria-hidden="true">&rarr;</span></a>
+    </div>
+    <dl class="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
+        <div class="flex flex-col-reverse">
+            <dt class="text-base leading-7 text-gray-300">TT Hồ Xá, Vĩnh Linh, Quảng Trị</dt>
+            <dd class="text-2xl font-bold leading-9 tracking-tight text-white">06 Ngô Quyền</dd>
+        </div>
+        <div class="flex flex-col-reverse">
+            <dt class="text-base leading-7 text-gray-300">Zalo/Số điện thoại</dt>
+            <dd class="text-2xl font-bold leading-9 tracking-tight text-white"><%= zalo %></dd>
+        </div>
+    </dl>
+</div>
